05867fc83225b473c267e5033509580d125bf7ec
Kristofer <kroldan@ford.com>
2022-03-23 16:35:02 -0400

    Add function definition to Exercise 2

--- 2.js
+++ 2.js
@@ -8,10 +8,9 @@ Given an array containing the english alphabet, write code that returns an array
 
 function getAlphaNumericAlphabet(){
   const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  let alphaNumericAlphabet
-
-  // Your code here
-
+  alphabet.unshift(0,1,2,3,4,5,6,7,8,9);
+  alphabet.push("&");
+  let alphaNumericAlphabet = alphabet
   return alphaNumericAlphabet
 }
 
